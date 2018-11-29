67ae7308b8e77e2760fd38258b2528e7156fee12
Rafał Rudol <rrrrudol@gmail.com>
2018-11-29 20:38:56 +0100

    add email

--- src/components/TopBar.js
+++ src/components/TopBar.js
@@ -4,7 +4,7 @@ export const TopBar = () => (
   <div className="home-topbar">
     <img src="img/logo.svg" alt="Logo" className="home-logo" />
 
-    <a href="#" className="button button-alt">
+    <a href="mailto:kontakt@akai.org.pl" className="button button-alt">
       Kontakt
     </a>
   </div>
